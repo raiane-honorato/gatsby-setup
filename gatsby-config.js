e1cf411020db1ed56dfad5b280076d5393ccb3e8
Raiane Honorato <raianehonorato16@gmail.com>
2021-10-16 12:03:00 -0300

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,70 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Portal Lacrei`,
+    siteUrl: `https://www.portallacrei.com.br`,
+    description: `Portal Lacrei Institucional`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
+
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+      },
+    },
+
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Portal Lacrei`,
+        short_name: `Portal Lacrei`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/assets/android-chrome-512x512.png` //512x512
+      },
+    },
+
+
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://www.portallacrei.com`,
+      },
+    },
+
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: process.env.NODE_ENV != 'production',
+      },
+    },
+
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        ignore: [`**/styles.js`],
+      },
+    },
+
+    `gatsby-plugin-offline`,
+
+  ],
 }
